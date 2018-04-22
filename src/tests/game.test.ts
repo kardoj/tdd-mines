18b0a728817d83560d70c1f4c262faed71294fcb
Kardo Jõeleht <kardoj@gmail.com>
2018-04-22 22:48:04 +0300

    Adjust fn name to be more descriptive

--- src/tests/game.test.ts
+++ src/tests/game.test.ts
@@ -2,12 +2,12 @@ import { CellContent, Game } from '../components/game';
 
 test('start sets game board', () => {
     const game = new Game();
-    game.start(oneMineTopCenter());
+    game.start(oneMineTopCenterBoard());
 
     expect(game.board).not.toBe(null);
 });
 
-function oneMineTopCenter(): Array<CellContent> {
+function oneMineTopCenterBoard(): Array<CellContent> {
     return [
         CellContent.One, CellContent.Mine, CellContent.One,
         CellContent.One, CellContent.One, CellContent.One,
