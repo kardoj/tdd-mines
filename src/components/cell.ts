481a152455d9b7589585a1e47fc4e7afa76f5d75
Kardo Jõeleht <kardoj@gmail.com>
2018-04-24 12:57:54 +0300

    Reorder methods and remove an export

--- src/components/cell.ts
+++ src/components/cell.ts
@@ -25,13 +25,13 @@ class Cell {
         this.status = CellStatus.Closed;
     }
 
-    isClosed(): boolean { return this.status === CellStatus.Closed; }
     open() { this.status = CellStatus.Opened; }
     close() { this.status = CellStatus.Closed; }
+    isClosed(): boolean { return this.status === CellStatus.Closed; }
     isAMine(): boolean { return this.content === CellContent.Mine; }
     isEmpty(): boolean { return this.content === CellContent.Empty; }
     isANumber(): boolean { return this.content !== CellContent.Empty && this.content !== CellContent.Mine; }
 }
 
-export { CellContent, CellStatus, Cell };
+export { CellContent, Cell };
 export default Cell;
