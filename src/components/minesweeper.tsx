4edbea98b36d1ae84e43046bef5f1e0da761344a
Kardo Jõeleht <kardoj@gmail.com>
2018-04-24 21:06:19 +0300

    Work with game UI

--- src/components/minesweeper.tsx
+++ src/components/minesweeper.tsx
@@ -1,10 +1,33 @@
 import * as React from 'react';
+import BoardGenerator from './boardgenerator';
+import Game from './game';
 
 /** This is the main visual component for the game */
 class Minesweeper extends React.Component {
+  state: any;
+
+  constructor(props: any) {
+    super(props);
+
+    const cols = 10;
+    const mines = 20;
+    const board = BoardGenerator.generate(cols, 10, mines);
+    
+    this.state = {
+      board, 
+      cols,
+      game: new Game(10, board),
+      mines
+    };
+  }
+
   render() {
     return (
-      <div>This is Minesweeper</div>
+      <table>
+        <tbody>
+          { /* TODO */ }
+        </tbody>
+      </table>
     );
   }
 }
